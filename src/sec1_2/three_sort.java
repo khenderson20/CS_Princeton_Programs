f7b27193917bc1888b88e7d054ff75bdd02b8daf
Kevin <kevin.d.hende@gmail.com>
2022-01-04 11:33:09 -0600

    three sort program completed

--- src/sec1_2/three_sort.java
+++ src/sec1_2/three_sort.java
@@ -14,6 +14,15 @@ public class three_sort {
         Scanner input = new Scanner(System.in);
 
         int n1, n2, n3;
+        System.out.print("Enter three integers in any order:");
+        n1 = input.nextInt();
+        n2 = input.nextInt();
+        n3 = input.nextInt();
 
+        int min = Math.min(n1, Math.min(n2, n3));
+        int max = Math.max(n1, Math.max(n2, n3));
+        int median = n1 + n2 + n3 - min - max;
+
+        System.out.printf("%d %d %d", min, median, max);
     }
 }
